dd1e2da188549317349d69e7a7187db8d517c3c7
grodriges <gleison_rodrigues@yahoo.com.br>
2020-06-25 00:11:52 -0300

    Criação do Front

--- CadastroUsuario-APP/src/app/Head/Head.component.ts
+++ CadastroUsuario-APP/src/app/Head/Head.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 @Component({
   selector: 'app-Head',
   templateUrl: './Head.component.html',
   styleUrls: ['./Head.component.css']
 })
 export class HeadComponent implements OnInit {
-
+  @Output() sidenavClose = new EventEmitter();
+ 
   constructor() { }
-
+ 
   ngOnInit() {
+  }
+ 
+  public onSidenavClose = () => {
+    this.sidenavClose.emit();
   }
 
 }
--- CadastroUsuario-APP/src/app/app.module.ts
+++ CadastroUsuario-APP/src/app/app.module.ts
@@ -5,20 +5,58 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CadastroComponent } from './Cadastro/Cadastro.component';
 import { HeadComponent } from './Head/Head.component';
-import { ListaCadastroComponent } from './ListaCadastro/ListaCadastro.component';
+// import { ListaCadastroComponent } from './ListaCadastro/ListaCadastro.component';
 import { FooterComponent } from './Footer/Footer.component';
 
+import {MatInputModule} from '@angular/material/input';
+import {MatTableModule} from '@angular/material/table';
+import {MatBottomSheetModule} from '@angular/material/bottom-sheet';
+import {MatButtonModule} from '@angular/material/button';
+import {MatButtonToggleModule} from '@angular/material/button-toggle';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatSliderModule} from '@angular/material/slider';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { MatListModule } from '@angular/material/list';
+import {MatIconModule} from '@angular/material/icon';
+import {FormsModule} from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TableStickyColumnsExample } from './ListaCadastro/ListaCadastro.component';
 @NgModule({
    declarations: [
       AppComponent,
       CadastroComponent,
       HeadComponent,
-      ListaCadastroComponent,
+      // ListaCadastroComponent,
+      TableStickyColumnsExample,
       FooterComponent
    ],
    imports: [
       BrowserModule,
-      AppRoutingModule
+      AppRoutingModule,
+      FormsModule,
+      BrowserAnimationsModule,
+      MatInputModule,
+      MatTableModule,
+      MatBottomSheetModule,
+      MatButtonModule,
+      MatButtonToggleModule,
+      MatSidenavModule,
+      MatSliderModule,
+      MatToolbarModule,
+      MatListModule,
+      MatIconModule
+   ],
+   exports: [
+      MatInputModule,
+      MatTableModule,
+      MatBottomSheetModule,
+      MatButtonModule,
+      MatButtonToggleModule,
+      MatSidenavModule,
+      MatSliderModule,
+      MatToolbarModule,
+      MatListModule,
+      MatIconModule
    ],
    providers: [],
    bootstrap: [
